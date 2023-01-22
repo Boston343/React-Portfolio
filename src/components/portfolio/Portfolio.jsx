5e49b816da0668b9f13c9c64066d3fc165c334bb
Boston343 <branden.austin1@gmail.com>
2023-01-22 15:59:14 -0500

    working on testimonials. Before adding swiper js

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,9 +3,9 @@ import "./portfolio.css";
 import img1 from "../../assets/portfolio1.jpg";
 import img2 from "../../assets/portfolio2.jpg";
 import img3 from "../../assets/portfolio3.jpg";
-import img4 from "../../assets/portfolio4.jpg";
-import img5 from "../../assets/portfolio5.jpg";
-import img6 from "../../assets/portfolio6.jpg";
+// import img4 from "../../assets/portfolio4.jpg";
+// import img5 from "../../assets/portfolio5.jpg";
+// import img6 from "../../assets/portfolio6.jpg";
 
 const data = [
   {
@@ -37,7 +37,7 @@ function PortfolioItem({ image, title, github, demo }) {
   return (
     <article className="portfolio__item">
       <div className="portfolio__item-image">
-        <img src={image} alt="portfolio project 1" />
+        <img src={image} alt="portfolio project" />
       </div>
       <h3>{title}</h3>
       <div className="portfolio__item-cta">
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,60 @@
 import React from "react";
 import "./testimonials.css";
+import avatar1 from "../../assets/avatar1.jpg";
+import avatar2 from "../../assets/avatar2.jpg";
+import avatar3 from "../../assets/avatar3.jpg";
+import avatar4 from "../../assets/avatar4.jpg";
 
+const data = [
+  {
+    avatar: avatar1,
+    name: "Tina Snow",
+    review:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem facilis reprehenderit quibusdam quos. Harum cum nostrum tempora numquam! Neque adipisci quibusdam laborum optio voluptate debitis commodi est ex excepturi pariatur.",
+  },
+  {
+    avatar: avatar2,
+    name: "Shatta Wale",
+    review:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem facilis reprehenderit quibusdam quos. Harum cum nostrum tempora numquam! Neque adipisci quibusdam laborum optio voluptate debitis commodi est ex excepturi pariatur.",
+  },
+  {
+    avatar: avatar3,
+    name: "Kwame Despite",
+    review:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem facilis reprehenderit quibusdam quos. Harum cum nostrum tempora numquam! Neque adipisci quibusdam laborum optio voluptate debitis commodi est ex excepturi pariatur.",
+  },
+  {
+    avatar: avatar4,
+    name: "Nana Ama McBrown",
+    review:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem facilis reprehenderit quibusdam quos. Harum cum nostrum tempora numquam! Neque adipisci quibusdam laborum optio voluptate debitis commodi est ex excepturi pariatur.",
+  },
+];
+
+// =======================================================
+//                  Testimonials
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>;
+  return (
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <article key={index} className="testimonial">
+              <div className="client__avatar">
+                <img src={avatar} alt="testimonial avatar" />
+              </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">{review}</small>
+            </article>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Testimonials;
